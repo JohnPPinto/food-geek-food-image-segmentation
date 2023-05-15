369859a1d6ab0904a2060e103f5ee141bb1fa108
John Pinto <66053973+JohnPPinto@users.noreply.github.com>
2023-05-15 17:02:08 +0530

    Updating app files

--- demo/app.py
+++ demo/app.py
@@ -1,3 +1,150 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8e6741530e9b05c41c9b7360166939f6f39c4fe785317d9bed4c3db646ff6cdb
-size 5479
+import typing
+import glob
+import numpy as np
+from PIL import Image
+from io import BytesIO
+import json
+import gradio as gr
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+from starlette.responses import Response
+from predict import predict
+
+model_path = 'models/yolov8s-seg-v1.onnx'
+
+# ----- FastAPI ----- #
+
+app = FastAPI(title='Food Geek API',
+              description='''Upload any images of food and obtain predicted values out of the image, 
+                             return json and image result.''')
+
+# Prediction result - JSON format
+@app.post('/predict-to-json')
+async def api_predict_json(file: UploadFile = File(...)):
+    """
+    This API will take any food image file and return a json file of prediction result.
+    The prediction result will contains numpy.ndarray which are dumped into json format.
+    To convert it back into the numpy.ndarray, use ```numpy.asarray(json.loads(...)) # Replace ... with the variable.  
+    """
+    # Validating only image files
+    extension = file.filename.split('.')[-1] in ('jpg', 'jpeg', 'png')
+    if not extension:
+        return f'File "{file.filename}" must be of Image format "JPG", "JPEG" or "PNG"'
+    
+    # Reading the image file
+    content = await file.read()
+    image = np.asarray(Image.open(BytesIO(content)))
+    
+    # Getting predictions results
+    results = predict(image_array=image,
+                      model_path=model_path,
+                      conf_threshold=0.7)
+    
+    # Converting the results in json format
+    results['org_image'] = json.dumps(results['org_image'].tolist())
+    results['result_image'] = json.dumps(results['result_image'].tolist())
+    results['boxes'] = json.dumps(results['boxes'].tolist())
+    results['masks'] = json.dumps(results['masks'].tolist())
+    results['scores'] = json.dumps(results['scores'].tolist())
+    results['class_ids'] = json.dumps(results['class_ids'].tolist())
+    
+    return results
+
+# Prediction result - Image visualization
+@app.post('/predict-to-image')
+async def api_predict_image(file: UploadFile = File(...)):
+    """
+    This API takes any image file and applies prediction on the image.
+    Once the process is done, Resulting image with prediction will be
+    displayed as a png file.
+    """
+    # Validating only image files
+    extension = file.filename.split('.')[-1] in ('jpg', 'jpeg', 'png')
+    if not extension:
+        return f'File "{file.filename}" must be of Image format "JPG", "JPEG" or "PNG"'
+    
+    # Reading the image file
+    content = await file.read()
+    image = np.asarray(Image.open(BytesIO(content)))
+    
+    # Getting predictions results
+    results = predict(image_array=image,
+                      model_path=model_path,
+                      conf_threshold=0.7)
+    
+    # Converting the predicted image into PIL image
+    img_base64 = Image.fromarray(results['result_image'])
+    
+    # buffering a PNG file and returning it.
+    with BytesIO() as buf:
+        img_base64.save(buf, format='PNG')
+        img_bytes = buf.getvalue()
+    return Response(img_bytes, media_type='image/png')
+
+# ----- Gradio ----- #
+
+# Creating a predict function for the website
+def gradio_predict(img):  
+    # Getting the prediction result for the image
+    results = predict(image_array=img,
+                      model_path=model_path,
+                      conf_threshold=0.7)
+    
+    # formating the classes
+    class_list = []
+    for names in results['classes']:
+        class_list.append(names.replace('_', ' '))
+        
+    # Validating the result
+    if len(results['masks']) == 0:
+        return (img, [([0, 0, 0, 0], 'No Food Detected')])
+    else:
+        # Isolating the result for every mask
+        pred = []
+        for i in range(len(results['masks'])):
+            pred.append((results['masks'][i] / 2, class_list[results['class_ids'][i]]))
+        return (img, pred)
+
+# Creating a function when segment is selected
+def on_annot_select(evt: gr.SelectData):
+    return f'Selected Food: {evt.value}'
+
+# Creating a function to clear all data
+def on_clear_btn():
+    return None, None, None
+
+# Creating the UI
+with gr.Blocks(theme=gr.themes.Soft()) as demo:
+    # Header
+    gr.Markdown('<center><h1>Food Geek</h1></center>')
+    
+    # Body
+    with gr.Row():
+        # Image uploading
+        with gr.Column(min_width=768):
+            with gr.Box():
+                with gr.Column():
+                    input = gr.Image(type='numpy', 
+                                     label='Image')
+                    with gr.Row():
+                        btn_clear = gr.Button(value='Clear')
+                        btn_submit = gr.Button(value="Submit", 
+                                               variant='primary')
+                    gr.Examples(examples=glob.glob('examples/*.jpg'),
+                                inputs=input)
+        # Displaying resulted image
+        output = gr.AnnotatedImage(label='Result').style(height=512, width=512, color_map={'': ''})
+    
+    # Additional info textbox 
+    food_info_box = gr.Textbox(label='Food Info')
+    
+    # Footer
+    gr.Markdown('Made by John - [Github Link]()')
+    
+    # On selected event
+    btn_submit.click(fn=gradio_predict, inputs=input, outputs=output)
+    btn_clear.click(fn=on_clear_btn, inputs=None, outputs=[input, output, food_info_box])
+    output.select(fn=on_annot_select, inputs=None, outputs=food_info_box)
+
+# Mounding the gradio app on to the fastAPI app
+app = gr.mount_gradio_app(app=app, blocks=demo, path='/')
--- demo/engine.py
+++ demo/engine.py
@@ -1,3 +1,201 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:657f513bfc3d329f39762dfab31fbecd99e14aa33de6eed732959ff0b5f88522
-size 8820
+import math
+import cv2
+import numpy as np
+import onnxruntime
+import matplotlib.pyplot as plt
+from utils import rescale_boxes, bbox_yolo_to_pascal, clip_bbox, compute_nms, sigmoid, draw_detections
+
+class YoloSegPredict:
+    """
+    This class helps in loading the model, predicting an image and providing coords and mask 
+    array for the segmentation.
+    Parameters:
+        model_path: A string to the path directing towards the model location.
+        conf_threshold: A float in the range (0, 1) for the confidence scores.
+        iou_threshold: A float in the range (0, 1) for the Non maximum supression.
+        num_masks: An int that contains the value of the predicted mask value used by Yolov8.
+    """
+    def __init__(self, model_path, conf_threshold = 0.7, iou_threshold = 0.5, num_masks=32):
+        self.conf_threshold = conf_threshold
+        self.iou_threshold = iou_threshold
+        self.num_masks = num_masks
+        
+        # Initializing the model
+        self.initialize_model(model_path)
+    
+    def initialize_model(self, model_path):
+        EP_LIST = ['CUDAExecutionProvider', 'CPUExecutionProvider']
+        self.ort_session = onnxruntime.InferenceSession(model_path, 
+                                                        providers = EP_LIST)
+        # Get meta data from the model
+        self.get_meta_details()
+        self.get_input_details()
+        self.get_output_details()
+    
+    def get_meta_details(self):
+        # Getting the model meta data.
+        model_meta = self.ort_session.get_modelmeta()
+        self.class_dict = eval(model_meta.custom_metadata_map['names'])
+        self.class_list = list(self.class_dict.values())
+        return self.class_list
+    
+    def get_input_details(self):
+        # Getting the input data
+        model_inputs = self.ort_session.get_inputs()
+        self.input_names = [model_inputs[i].name for i in range(len(model_inputs))]
+        self.input_shape = model_inputs[0].shape
+        self.input_height = self.input_shape[2]
+        self.input_width = self.input_shape[3]
+        
+    def get_output_details(self):
+        # Getting the output data
+        model_outputs = self.ort_session.get_outputs()
+        self.output_names = [model_outputs[i].name for i in range(len(model_outputs))]
+        
+    def __call__(self, image):
+        return self.segment_objects(image)
+    
+    def segment_objects(self, image):
+        # Prepare the image array as a input tensor.
+        input_tensor, self.input_img_resized = self.prepare_input(image)
+        
+        # Perform inference on the image
+        outputs = self.inference(input_tensor)
+        
+        # Extract prediction data
+        self.boxes, self.scores, self.class_ids, mask_pred = self.process_box_output(outputs[0])
+        self.mask_maps = self.process_mask_output(mask_pred, outputs[1])
+        
+        return self.input_img_resized, self.boxes, self.scores, self.class_ids, self.mask_maps
+    
+    def prepare_input(self, image):
+        # Getting image info
+        self.image_height, self.image_width = image.shape[:2]
+        
+        # Resize input image to input size
+        input_img_resized = cv2.resize(image, (self.input_width, self.input_height))
+        
+        # Preprocessing the input image
+        input_img = input_img_resized / 255.0 # Normalizing
+        input_img = input_img.transpose(2, 0, 1)
+        input_tensor = input_img[np.newaxis, :, :, :].astype(np.float32)
+        
+        return input_tensor, input_img_resized
+    
+    def inference(self, input_tensor):
+        # Predicting using the Yolo onnx model
+        outputs = self.ort_session.run(self.output_names, {self.input_names[0]: input_tensor})
+        
+        return outputs
+    
+    def process_box_output(self, box_output):
+        # Extracting predictions from box outputs
+        predictions = np.squeeze(box_output).T
+        num_classes = box_output.shape[1] - self.num_masks - 4 # box data - mask data - box coords
+        
+        # Filter out confidence scores below threshold
+        scores = np.max(predictions[:, 4:4+num_classes], axis=1)
+        predictions = predictions[scores > self.conf_threshold, :]
+        scores = scores[scores > self.conf_threshold]
+        
+        # Validating for no scores
+        if len(scores) == 0:
+            return [], [], [], np.array([])
+        
+        # Seprating the prediction from the first output
+        box_predictions = predictions[..., :num_classes+4]
+        mask_predictions = predictions[..., num_classes+4:]
+        
+        # Getting class with the highest confidense score
+        class_ids = np.argmax(box_predictions[:, 4:], axis=1)
+        
+        # Getting the bounding box for all the objects
+        boxes = self.extract_boxes(box_predictions)
+        
+        # Apply Non Maximum Suooression to suppress overlapping box
+        indices = compute_nms(boxes=boxes, 
+                              scores=scores, 
+                              iou_threshold=self.iou_threshold)
+        return boxes[indices], scores[indices], class_ids[indices], mask_predictions[indices]
+    
+    def extract_boxes(self, box_predictions):
+        # Extract box from predictions
+        boxes = box_predictions[:, :4]
+        
+        # Scale boxes to original image dimension
+        boxes = rescale_boxes(boxes=boxes, 
+                              input_shape=(self.input_height, self.input_width), 
+                              output_shape=(self.image_height, self.image_width))
+        
+        # Convert the boxes to pascal voc format
+        boxes = bbox_yolo_to_pascal(boxes=boxes)
+        
+        # Clipping the boxes range to a image limit
+        boxes = clip_bbox(boxes=boxes, 
+                          height=self.image_height, 
+                          width=self.image_width)
+        
+        return boxes
+    
+    def process_mask_output(self, mask_predictions, mask_output):
+        # if no mask prediction
+        if mask_predictions.shape[0] == 0:
+            return []
+        
+        mask_output = np.squeeze(mask_output)
+        
+        # Calculate the mask area for all the box
+        num_mask, mask_height, mask_width = mask_output.shape
+        masks = sigmoid(mask_predictions @ mask_output.reshape((num_mask, -1)))
+        masks = masks.reshape((-1, mask_height, mask_width))
+        
+        # Rescale the boxes to match the mask size
+        scale_boxes = rescale_boxes(boxes=self.boxes,
+                                    input_shape=(self.image_height, self.image_width),
+                                    output_shape=(mask_height, mask_width))
+        
+        # Mask map for each box and mask pair
+        mask_maps = np.zeros((len(scale_boxes), self.image_height, self.image_width))
+        blur_size = (int(self.image_width/mask_width), int(self.image_height/mask_height))
+        for i in range(len(scale_boxes)):
+            # Rounding the scaled boxes
+            scale_x1 = int(math.floor(scale_boxes[i][0]))
+            scale_y1 = int(math.floor(scale_boxes[i][1]))
+            scale_x2 = int(math.ceil(scale_boxes[i][2]))
+            scale_y2 = int(math.ceil(scale_boxes[i][3]))
+            
+            # Rounding the base boxes
+            x1 = int(math.floor(self.boxes[i][0]))
+            y1 = int(math.floor(self.boxes[i][1]))
+            x2 = int(math.ceil(self.boxes[i][2]))
+            y2 = int(math.ceil(self.boxes[i][3]))
+            
+            # Cropping the scaled mask and resizing it to image dimension
+            scale_crop_mask = masks[i][scale_y1: scale_y2, scale_x1: scale_x2]
+            crop_mask = cv2.resize(scale_crop_mask, 
+                                   (x2 - x1, y2 - y1), 
+                                   interpolation=cv2.INTER_CUBIC)
+            crop_mask = cv2.blur(crop_mask, blur_size)
+            crop_mask = (crop_mask > 0.5).astype(np.uint8)
+            mask_maps[i, y1:y2, x1:x2] = crop_mask
+        
+        return mask_maps
+    
+    def draw_bbox(self, image, mask_alpha=0.5):
+        # Drawing only the bounding box and filling it.
+        return draw_detections(image=image,
+                               boxes=self.boxes,
+                               scores=self.scores,
+                               class_ids=self.class_ids,
+                               class_list=self.class_list,
+                               mask_alpha=mask_alpha)
+    
+    def draw_masks(self, image, mask_alpha=0.5):
+        # Drawing both the bounding box and the mask
+        return draw_detections(image=image,
+                               boxes=self.boxes,
+                               scores=self.scores,
+                               class_ids=self.class_ids,
+                               class_list=self.class_list,
+                               mask_alpha=mask_alpha,
+                               mask_maps=self.mask_maps)
